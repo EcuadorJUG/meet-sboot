5b716eed2df845f7461330181d5cc6132e320827
Nel Gonzalez <nelzonleonardo@gmail.com>
2017-03-15 21:52:26 -0500

    refactor

--- src/main/java/ng/springboot/football/MeetFirebaseApp.java
+++ src/main/java/ng/springboot/football/MeetFirebaseApp.java
@@ -25,6 +25,7 @@ public class MeetFirebaseApp {
             .setDatabaseUrl("https://meet-spring-boot.firebaseio.com")
             .build();
         FirebaseApp.initializeApp(options);
+
         // listeners
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         listenResults(database);
@@ -32,25 +33,24 @@ public class MeetFirebaseApp {
 
     private void listenResults(FirebaseDatabase database) {
         DatabaseReference resultsRef = database.getReference("/server/results");
-
         resultsRef.addChildEventListener(new ChildEventListener() {
                 @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String previousKey) {
-                    handleNewResult(database, dataSnapshot);
+                public void onChildAdded(DataSnapshot snapshot, String previousKey) {
+                    handleNewResult(database, snapshot);
                 }
 
                 @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String previousKey) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String previousKey) {
+                public void onChildChanged(DataSnapshot snapshot, String previousKey) {
 
                 }
 
                 @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                public void onChildMoved(DataSnapshot snapshot, String previousKey) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot snapshot) {
 
                 }
 
@@ -61,16 +61,16 @@ public class MeetFirebaseApp {
             });
     }
 
-    private void handleNewResult(FirebaseDatabase database, DataSnapshot dataSnapshot) {
-        EventResult eventResult = dataSnapshot.getValue(EventResult.class);
-        String eventResultKey = dataSnapshot.getKey();
-        System.out.println("result: " + eventResult);
-        DatabaseReference betsRef = database.getReference("/public/bets").child(dataSnapshot.getKey());
-        Query winners = betsRef.orderByChild("prediction").equalTo(eventResult.getResult());
+    private void handleNewResult(FirebaseDatabase database, DataSnapshot snapshot) {
+        EventResult result = snapshot.getValue(EventResult.class);
+        String resultKey = snapshot.getKey();
+        System.out.println("result: " + result);
+        DatabaseReference betsRef = database.getReference("/public/bets").child(snapshot.getKey());
+        Query winners = betsRef.orderByChild("prediction").equalTo(result.getResult());
         winners.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    handleWinners(database, dataSnapshot, eventResult, eventResultKey);
+                public void onDataChange(DataSnapshot snapshot) {
+                    handleWinners(database, snapshot, result, resultKey);
                 }
 
                 @Override
@@ -80,27 +80,31 @@ public class MeetFirebaseApp {
             });
     }
 
-    private void handleWinners(FirebaseDatabase database, DataSnapshot dataSnapshot,
-                               EventResult eventResult, String eventResultKey) {
+    private void handleWinners(FirebaseDatabase database, DataSnapshot snapshot, EventResult result, String resultKey) {
         Map<String, BetWinner> winners = new HashMap<String, BetWinner>();
-        dataSnapshot.getChildren().forEach(c -> {
+        snapshot.getChildren().forEach(c -> {
                 Bet bet = c.getValue(Bet.class);
                 System.out.println("bet: " + bet);
                 winners.put(c.getKey(), new BetWinner(bet.getName(), bet.getEmail()));
             });
-
         Map<String, Object> winnersUpdates = new HashMap<String, Object>();
-        winnersUpdates.put("/users/" + eventResult.getOwner() + "/events/" + eventResultKey + "/status", "finished");
-        winnersUpdates.put("/users/" + eventResult.getOwner() + "/events/" + eventResultKey + "/winners", dataSnapshot.getChildrenCount());
-        winnersUpdates.put("/users/" + eventResult.getOwner() + "/events/" + eventResultKey + "/resultA", eventResult.getResultA());
-        winnersUpdates.put("/users/" + eventResult.getOwner() + "/events/" + eventResultKey + "/resultB", eventResult.getResultB());
-        winnersUpdates.put("/public/events/" + eventResultKey + "/status", "finished");
-        winnersUpdates.put("/public/events/" + eventResultKey + "/winners", dataSnapshot.getChildrenCount());
-        winnersUpdates.put("/public/events/" + eventResultKey + "/resultA", eventResult.getResultA());
-        winnersUpdates.put("/public/events/" + eventResultKey + "/resultB", eventResult.getResultB());
-        winnersUpdates.put("/public/winners/" + eventResultKey, winners);
-        winnersUpdates.put("/server/results/" + eventResultKey, null);
+
+        String userEventPrefix = "/users/" + result.getOwner() + "/events/" + resultKey;
+        winnersUpdates.put(userEventPrefix + "/status", "finished");
+        winnersUpdates.put(userEventPrefix + "/winners", snapshot.getChildrenCount());
+        winnersUpdates.put(userEventPrefix + "/resultA", result.getResultA());
+        winnersUpdates.put(userEventPrefix + "/resultB", result.getResultB());
+
+        String publicEventPrefix = "/public/events/" + resultKey;
+        winnersUpdates.put(publicEventPrefix + "/status", "finished");
+        winnersUpdates.put(publicEventPrefix + "/winners", snapshot.getChildrenCount());
+        winnersUpdates.put(publicEventPrefix + "/resultA", result.getResultA());
+        winnersUpdates.put(publicEventPrefix + "/resultB", result.getResultB());
+
+        winnersUpdates.put("/public/winners/" + resultKey, winners);
+
+        winnersUpdates.put("/server/results/" + resultKey, null);
+
         database.getReference().updateChildren(winnersUpdates);
     }
-
 }
